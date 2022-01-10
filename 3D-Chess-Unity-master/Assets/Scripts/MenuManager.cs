489edbfa7d3a002dcb415921f76a34422aff9bba
Indira kar <indira@girzla.co.in>
2022-01-10 18:24:57 +0530

    Credit Update

---
+++ 3D-Chess-Unity-master/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject creditCanvas;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credit()
+    {
+        MainMenu.SetActive(false);
+        creditCanvas.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            creditCanvas.SetActive(false);
+            MainMenu.SetActive(true);
+        }
+    }
+}
---
+++ 3D-Chess-Unity-master/Assets/Scripts/QuiteGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuiteGame : MonoBehaviour
+{
+    public GameObject quiteGameCanvas;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            quiteGameCanvas.SetActive(true);
+        }
+    }
+
+    public void Yes()
+    {
+        Application.Quit();
+    }
+
+    public void No()
+    {
+        quiteGameCanvas.SetActive(false);
+    }
+}
